08a9a5d501e7168828be7ef7ad474366d35d17c7
Calixte Denizet <calixte.denizet@gmail.com>
2019-08-05 12:09:00 +0200

    Replace ts-java by submodule

--- tree-sitter-java/index.js
+++
@@ -1,9 +0,0 @@
-try {
-  module.exports = require("./build/Release/tree_sitter_java_binding");
-} catch (error) {
-  try {
-    module.exports = require("./build/Debug/tree_sitter_java_binding");
-  } catch (_) {
-    throw error
-  }
-}
