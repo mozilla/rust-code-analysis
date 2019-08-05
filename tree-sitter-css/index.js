a16483548b5f9d0e9576185833af4e56cdcc6ae4
Calixte Denizet <calixte.denizet@gmail.com>
2019-08-05 12:26:32 +0200

    Replace ts-css by submodule

--- tree-sitter-css/index.js
+++
@@ -1,9 +0,0 @@
-try {
-  module.exports = require("./build/Release/tree_sitter_css_binding");
-} catch (error) {
-  try {
-    module.exports = require("./build/Debug/tree_sitter_css_binding");
-  } catch (_) {
-    throw error
-  }
-}
