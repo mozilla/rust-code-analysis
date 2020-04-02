8efaf2ffa7d3a104006ee3d2f0faf73783590c5a
Luni-4 <luni-4@hotmail.it>
2020-04-02 15:32:26 +0200

    Update tests

--- src/nom.rs
+++ src/nom.rs
@@ -193,3 +193,63 @@ impl Nom for JavaCode {}
 impl Nom for GoCode {}
 impl Nom for CssCode {}
 impl Nom for HtmlCode {}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use super::*;
+
+    #[test]
+    fn test_nom_python() {
+        let sample =
+            String::from("def a():\n    pass\n\ndef b():\n    pass\n\ndef c():\n    pass\n");
+        let path = PathBuf::from("foo.py");
+        let b_sample = sample.as_bytes().to_vec();
+        let parser = PythonParser::new(b_sample.clone(), &path, None);
+        let func_space = metrics(&parser, &path).unwrap();
+
+        assert_eq!(func_space.metrics.nom.functions() as usize, 3);
+        assert_eq!(func_space.metrics.nom.closures() as usize, 0);
+        assert_eq!(func_space.metrics.nom.total() as usize, 3);
+    }
+
+    #[test]
+    fn test_nom_rust() {
+        let sample = String::from("mod A { fn foo() {}}\n mod B { fn foo() {}}\n");
+        let path = PathBuf::from("foo.rs");
+        let b_sample = sample.as_bytes().to_vec();
+        let parser = RustParser::new(b_sample.clone(), &path, None);
+        let func_space = metrics(&parser, &path).unwrap();
+
+        assert_eq!(func_space.metrics.nom.functions() as usize, 2);
+        assert_eq!(func_space.metrics.nom.closures() as usize, 0);
+        assert_eq!(func_space.metrics.nom.total() as usize, 2);
+    }
+
+    #[test]
+    fn test_nom_cpp() {
+        let sample = String::from("struct A {\n  void foo(int) {}\n  void foo(double) {}\n};\n");
+        let path = PathBuf::from("foo.cpp");
+        let b_sample = sample.as_bytes().to_vec();
+        let parser = CppParser::new(b_sample.clone(), &path, None);
+        let func_space = metrics(&parser, &path).unwrap();
+
+        assert_eq!(func_space.metrics.nom.functions() as usize, 2);
+        assert_eq!(func_space.metrics.nom.closures() as usize, 0);
+        assert_eq!(func_space.metrics.nom.total() as usize, 2);
+    }
+
+    #[test]
+    fn test_nom_c() {
+        let sample = String::from("int foo();\n\nint foo() {\n  return 0;\n}\n");
+        let path = PathBuf::from("foo.c");
+        let b_sample = sample.as_bytes().to_vec();
+        let parser = CppParser::new(b_sample.clone(), &path, None);
+        let func_space = metrics(&parser, &path).unwrap();
+
+        assert_eq!(func_space.metrics.nom.functions() as usize, 1);
+        assert_eq!(func_space.metrics.nom.closures() as usize, 0);
+        assert_eq!(func_space.metrics.nom.total() as usize, 1);
+    }
+}
