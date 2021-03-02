285e0d8c4af7e24202fe4c18c3a7be78c4f3cf32
Luni-4 <luni-4@hotmail.it>
2021-03-02 15:47:55 +0100

    Make tree-sitter-mozjs independent
    In this way tree-sitter-mozjs is independent from tree-sitter-javascript

--- tree-sitter-mozjs/src/scanner.c
+++ tree-sitter-mozjs/src/scanner.c
@@ -1,6 +1,6 @@
 #include <tree_sitter/parser.h>
 
-extern bool tree_sitter_javascript_external_scanner_scan(void *payload, TSLexer *lexer, const bool *valid_symbols);
+#include "./tree_sitter_javascript_scanner.c"
 
 void *tree_sitter_mozjs_external_scanner_create() { return NULL; }
 void tree_sitter_mozjs_external_scanner_destroy(void *p) {}
@@ -9,5 +9,5 @@ unsigned tree_sitter_mozjs_external_scanner_serialize(void *p, char *buffer) { r
 void tree_sitter_mozjs_external_scanner_deserialize(void *p, const char *b, unsigned n) {}
 
 bool tree_sitter_mozjs_external_scanner_scan(void *payload, TSLexer *lexer, const bool *valid_symbols) {
-    return tree_sitter_javascript_external_scanner_scan(payload, lexer, valid_symbols);
+    return tree_sitter_javascript_external_scanner_mozjs_scan(payload, lexer, valid_symbols);
 }
