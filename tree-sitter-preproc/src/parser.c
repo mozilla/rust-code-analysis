e7615789929cb32026dd94fd03e7c3c3bc81c16a
Luni-4 <luni-4@hotmail.it>
2021-03-16 12:55:52 +0100

    Regenerate tree-sitter-preproc grammar

--- tree-sitter-preproc/src/parser.c
+++ tree-sitter-preproc/src/parser.c
@@ -5,7 +5,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 12
+#define LANGUAGE_VERSION 13
 #define STATE_COUNT 121
 #define LARGE_STATE_COUNT 16
 #define SYMBOL_COUNT 36
@@ -14,6 +14,7 @@
 #define EXTERNAL_TOKEN_COUNT 1
 #define FIELD_COUNT 0
 #define MAX_ALIAS_SEQUENCE_LENGTH 5
+#define PRODUCTION_ID_COUNT 1
 
 enum {
   sym_identifier = 1,
@@ -278,7 +279,7 @@ static const TSSymbolMetadata ts_symbol_metadata[] = {
   },
 };
 
-static TSSymbol ts_alias_sequences[1][MAX_ALIAS_SEQUENCE_LENGTH] = {
+static TSSymbol ts_alias_sequences[PRODUCTION_ID_COUNT][MAX_ALIAS_SEQUENCE_LENGTH] = {
   [0] = {0},
 };
 
@@ -408,12 +409,12 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
       END_STATE();
     case 14:
       if (lookahead == '"') ADVANCE(108);
-      if (lookahead == '\\') ADVANCE(68);
+      if (lookahead == '\\') ADVANCE(69);
       if (lookahead != 0) ADVANCE(14);
       END_STATE();
     case 15:
       if (lookahead == '\'') ADVANCE(109);
-      if (lookahead == '\\') ADVANCE(69);
+      if (lookahead == '\\') ADVANCE(70);
       if (lookahead != 0) ADVANCE(15);
       END_STATE();
     case 16:
@@ -620,15 +621,15 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
           lookahead == ' ') ADVANCE(100);
       END_STATE();
     case 68:
-      if (lookahead != 0) ADVANCE(14);
-      END_STATE();
-    case 69:
-      if (lookahead != 0) ADVANCE(15);
-      END_STATE();
-    case 70:
       if (lookahead != 0 &&
           lookahead != '\r') ADVANCE(113);
       if (lookahead == '\r') ADVANCE(114);
+      END_STATE();
+    case 69:
+      if (lookahead != 0) ADVANCE(14);
+      END_STATE();
+    case 70:
+      if (lookahead != 0) ADVANCE(15);
       END_STATE();
     case 71:
       if (eof) ADVANCE(73);
@@ -933,7 +934,7 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
       END_STATE();
     case 113:
       ACCEPT_TOKEN(sym_comment);
-      if (lookahead == '\\') ADVANCE(70);
+      if (lookahead == '\\') ADVANCE(68);
       if (lookahead != 0 &&
           lookahead != '\n') ADVANCE(113);
       END_STATE();
@@ -941,7 +942,7 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
       ACCEPT_TOKEN(sym_comment);
       if (lookahead != 0 &&
           lookahead != '\\') ADVANCE(113);
-      if (lookahead == '\\') ADVANCE(70);
+      if (lookahead == '\\') ADVANCE(68);
       END_STATE();
     default:
       return false;
@@ -2814,13 +2815,21 @@ extern const TSLanguage *tree_sitter_preproc(void) {
     .alias_count = ALIAS_COUNT,
     .token_count = TOKEN_COUNT,
     .external_token_count = EXTERNAL_TOKEN_COUNT,
+    .state_count = STATE_COUNT,
+    .large_state_count = LARGE_STATE_COUNT,
+    .production_id_count = PRODUCTION_ID_COUNT,
+    .field_count = FIELD_COUNT,
+    .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
+    .parse_table = (const uint16_t *)ts_parse_table,
+    .small_parse_table = (const uint16_t *)ts_small_parse_table,
+    .small_parse_table_map = (const uint32_t *)ts_small_parse_table_map,
+    .parse_actions = ts_parse_actions,
     .symbol_names = ts_symbol_names,
     .symbol_metadata = ts_symbol_metadata,
-    .parse_table = (const uint16_t *)ts_parse_table,
-    .parse_actions = ts_parse_actions,
-    .lex_modes = ts_lex_modes,
+    .public_symbol_map = ts_symbol_map,
+    .alias_map = ts_non_terminal_alias_map,
     .alias_sequences = (const TSSymbol *)ts_alias_sequences,
-    .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
+    .lex_modes = ts_lex_modes,
     .lex_fn = ts_lex,
     .external_scanner = {
       (const bool *)ts_external_scanner_states,
@@ -2831,13 +2840,6 @@ extern const TSLanguage *tree_sitter_preproc(void) {
       tree_sitter_preproc_external_scanner_serialize,
       tree_sitter_preproc_external_scanner_deserialize,
     },
-    .field_count = FIELD_COUNT,
-    .large_state_count = LARGE_STATE_COUNT,
-    .small_parse_table = (const uint16_t *)ts_small_parse_table,
-    .small_parse_table_map = (const uint32_t *)ts_small_parse_table_map,
-    .public_symbol_map = ts_symbol_map,
-    .alias_map = ts_non_terminal_alias_map,
-    .state_count = STATE_COUNT,
   };
   return &language;
 }
