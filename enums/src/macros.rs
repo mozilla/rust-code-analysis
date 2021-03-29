fabfe451bdd24fbc2ee21f09ac6914f2742d0a66
Luni-4 <luni-4@hotmail.it>
2021-03-29 16:36:12 +0200

    Delete Python submodule

--- enums/src/macros.rs
+++ enums/src/macros.rs
@@ -20,6 +20,7 @@ macro_rules! mk_get_language {
                   LANG::Typescript => tree_sitter_typescript::language_typescript(),
                   LANG::Tsx => tree_sitter_typescript::language_tsx(),
                   LANG::Javascript => tree_sitter_javascript::language(),
+                  LANG::Python => tree_sitter_python::language(),
                   LANG::Preproc => tree_sitter_preproc::language(),
                   LANG::Ccomment => tree_sitter_ccomment::language(),
                   LANG::Cpp => tree_sitter_mozcpp::language(),
