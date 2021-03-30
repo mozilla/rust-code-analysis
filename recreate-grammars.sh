e22301b949070d2421d03917234dae33c904df04
Luni-4 <luni-4@hotmail.it>
2021-03-30 23:41:13 +0200

    Clarify the script to recreate the grammars

--- recreate-grammars.sh
+++ recreate-grammars.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Recreate all grammars
+# Enter enums directory
 pushd enums
+
+# Recreate all grammars
 cargo clean && cargo run -- -lrust -o ../src/languages
+
+# Exit enums directory
 popd
 
-# Format the code
+# Format the code of the recreated grammars
 cargo fmt
