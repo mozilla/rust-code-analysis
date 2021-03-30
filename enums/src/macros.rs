1bf2a82025ef15332835bfddf305a8313228e904
Luni-4 <luni-4@hotmail.it>
2021-03-30 12:30:07 +0200

    enums: Format the code

--- enums/src/macros.rs
+++ enums/src/macros.rs
@@ -15,18 +15,18 @@ macro_rules! mk_enum {
 macro_rules! mk_get_language {
     ( $( ($camel:ident, $name:ident) ),* ) => {
         pub fn get_language(lang: &LANG) -> Language {
-              match lang {
-                  LANG::Java => tree_sitter_java::language(),
-                  LANG::Typescript => tree_sitter_typescript::language_typescript(),
-                  LANG::Tsx => tree_sitter_typescript::language_tsx(),
-                  LANG::Javascript => tree_sitter_javascript::language(),
-                  LANG::Python => tree_sitter_python::language(),
-                  LANG::Rust => tree_sitter_rust::language(),
-                  LANG::Preproc => tree_sitter_preproc::language(),
-                  LANG::Ccomment => tree_sitter_ccomment::language(),
-                  LANG::Cpp => tree_sitter_mozcpp::language(),
-                  LANG::Mozjs => tree_sitter_mozjs::language(),
-              }
+            match lang {
+                LANG::Java => tree_sitter_java::language(),
+                LANG::Typescript => tree_sitter_typescript::language_typescript(),
+                LANG::Tsx => tree_sitter_typescript::language_tsx(),
+                LANG::Javascript => tree_sitter_javascript::language(),
+                LANG::Python => tree_sitter_python::language(),
+                LANG::Rust => tree_sitter_rust::language(),
+                LANG::Preproc => tree_sitter_preproc::language(),
+                LANG::Ccomment => tree_sitter_ccomment::language(),
+                LANG::Cpp => tree_sitter_mozcpp::language(),
+                LANG::Mozjs => tree_sitter_mozjs::language(),
+            }
         }
     };
 }
