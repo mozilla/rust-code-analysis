01abf8703707465513a78aadcc833bb9e1b9912e
Luni-4 <luni-4@hotmail.it>
2021-04-21 14:16:37 +0200

    Simplify script to recreate grammars (#624)

--- recreate-grammars.sh
+++ recreate-grammars.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Enter enums directory
-pushd enums
+# Clean old grammars builds
+cargo clean --manifest-path ./enums/Cargo.toml
 
 # Recreate all grammars
-cargo clean && cargo run -- -lrust -o ../src/languages
-
-# Exit enums directory
-popd
+cargo run --manifest-path ./enums/Cargo.toml -- -lrust -o ./src/languages
 
 # Format the code of the recreated grammars
 cargo fmt
