e12f7c0e382e77eb4166ff44ea6ad67f4806c417
Luni-4 <luni-4@hotmail.it>
2021-05-17 15:23:24 +0200

    Add a way to print the AST nodes of a grammar as strings
    The previous code was unused, but this new one allows to print the
    AST nodes of a grammar as strings

--- enums/src/rust.rs
+++ enums/src/rust.rs
@@ -3,7 +3,7 @@ extern crate phf_codegen;
 use askama::Template;
 use enum_iterator::IntoEnumIterator;
 use std::fs::File;
-use std::io::{Cursor, Write};
+use std::io::Write;
 use std::path::PathBuf;
 
 use crate::common::*;
@@ -14,7 +14,6 @@ use crate::languages::*;
 struct RustTemplate {
     c_name: String,
     names: Vec<(String, bool, String)>,
-    phf_map: String,
 }
 
 pub fn generate_rust(output: &str, file_template: &str) -> std::io::Result<()> {
@@ -29,21 +28,7 @@ pub fn generate_rust(output: &str, file_template: &str) -> std::io::Result<()> {
 
         let names = get_token_names(&language, false);
 
-        let mut phf_map = Cursor::new(Vec::new());
-        let mut builder = phf_codegen::Map::new();
-        for (name, dup, ts_name) in names.iter() {
-            if !dup {
-                builder.entry(ts_name.as_str(), format!("{}::{}", c_name, name).as_str());
-            }
-        }
-        writeln!(phf_map, "{}", builder.build()).unwrap();
-        let phf_map = String::from_utf8(phf_map.into_inner()).unwrap();
-
-        let args = RustTemplate {
-            c_name,
-            names,
-            phf_map,
-        };
+        let args = RustTemplate { c_name, names };
 
         file.write_all(args.render().unwrap().as_bytes())?;
     }
