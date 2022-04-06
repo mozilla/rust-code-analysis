1704c12984859aef582811e80afd4e2f9e24be67
Devon Burriss <devon@chimplab.co>
2022-04-06 06:53:14 +0200

    Operands impl and assertions

--- src/getter.rs
+++ src/getter.rs
@@ -532,16 +532,14 @@ impl Getter for JavaCode {
             | PLUS | DASH | STAR | SLASH | AMP | PIPE | CARET | PERCENT| LTLT | GTGT | GTGTGT
             | PLUSEQ | DASHEQ | STAREQ | SLASHEQ | AMPEQ | PIPEEQ | CARETEQ | PERCENTEQ | LTLTEQ | GTGTEQ | GTGTGTEQ
             // type identifier
-            | TypeIdentifier 
-            // Literals
-            | IntegralType | FloatingPointType | BooleanType | NullLiteral
+            | TypeIdentifier | IntegralType | FloatingPointType | BooleanType
             => {
                 println!("operator: {}", node.object().kind().to_string());
                 HalsteadType::Operator
             },
 
-            // variables, constants
-            Identifier | Literal  => {
+            // Operands: variables, constants, literals
+            Identifier | NullLiteral | ClassLiteral | StringLiteral | CharacterLiteral | HexIntegerLiteral | OctalIntegerLiteral | BinaryIntegerLiteral | DecimalIntegerLiteral | HexFloatingPointLiteral | DecimalFloatingPointLiteral  => {
                 println!("operand: {} ", node.object().kind().to_string());
                 HalsteadType::Operand
             },
