762d12d451855c398718e2637f57bd999ea65f0e
Devon Burriss <devon@chimplab.co>
2022-04-13 19:26:56 +0200

    Adds test for ops

--- src/ops.rs
+++ src/ops.rs
@@ -648,4 +648,37 @@ mod tests {
             ],
         );
     }
+
+    #[test]
+    fn java_ops() {
+        check_ops(
+            LANG::Java,
+            "public class Main {
+                public static void main(String args[]) {
+                      int a, b, c, avg;
+                      a = 5; b = 5; c = 5;
+                      avg = (a + b + c) / 3;
+                      MessageFormat.format(\"{0}\", avg);
+                    }
+                }",
+            "foo.java",
+            &mut vec![
+                "{}", "void", "()", "[]", "String", ",", ";", "int", "=", "+", "/", "format",
+            ],
+            &mut vec![
+                "Main",
+                "main",
+                "args",
+                "a",
+                "b",
+                "c",
+                "avg",
+                "5",
+                "3",
+                "MessageFormat",
+                "format",
+                "0",
+            ],
+        );
+    }
 }
