c5f2db012ea94c035d530ba3e94e0911170b488f
Luni-4 <luni-4@hotmail.it>
2022-05-25 15:50:17 +0200

    web: Update command line interface

--- rust-code-analysis-web/src/main.rs
+++ rust-code-analysis-web/src/main.rs
@@ -5,54 +5,39 @@
 #![recursion_limit = "256"]
 mod web;
 
-use clap::{crate_version, App, Arg};
+use clap::Parser;
 
 use web::server;
 
+#[derive(Parser, Debug)]
+#[structopt(
+    name = "rust-code-analysis-web",
+    version,
+    author,
+    about = "Run a web server."
+)]
+struct Opts {
+    /// Number of jobs.
+    #[structopt(long, short = 'j')]
+    num_jobs: Option<usize>,
+    /// Host for the web server.
+    #[structopt(long, short, default_value = "127.0.0.1")]
+    host: String,
+    /// Port for the web server.
+    #[structopt(long, short, default_value = "8080")]
+    port: u16,
+}
+
 #[actix_web::main]
 async fn main() {
-    let matches = App::new("rust-code-analysis-web")
-        .version(crate_version!())
-        .author(&*env!("CARGO_PKG_AUTHORS").replace(':', "\n"))
-        .about("Run a web server")
-        .arg(
-            Arg::new("num_jobs")
-                .help("Number of jobs")
-                .short('j')
-                .value_name("NUMBER")
-                .default_value("")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::new("host")
-                .help("Host for the web server")
-                .long("host")
-                .default_value("127.0.0.1")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::new("port")
-                .help("Port for the web server")
-                .long("port")
-                .default_value("8080")
-                .takes_value(true),
-        )
-        .get_matches();
+    let opts = Opts::parse();
 
-    let num_jobs = if let Ok(num_jobs) = matches.value_of("num_jobs").unwrap().parse::<usize>() {
-        num_jobs
-    } else {
-        num_cpus::get()
-    };
+    let num_jobs = opts.num_jobs.unwrap_or_else(num_cpus::get);
 
-    let host = matches.value_of("host").unwrap();
-    let port = if let Ok(port) = matches.value_of("port").unwrap().parse::<u16>() {
-        port
-    } else {
-        eprintln!("Invalid port number");
-        return;
-    };
-    if let Err(e) = server::run(host.to_string(), port, num_jobs).await {
-        eprintln!("Cannot run the server at {}:{}: {}", host, port, e);
+    if let Err(e) = server::run(opts.host.clone(), opts.port, num_jobs).await {
+        eprintln!(
+            "Cannot run the server at {}:{}: {}",
+            opts.host, opts.port, e
+        );
     }
 }
