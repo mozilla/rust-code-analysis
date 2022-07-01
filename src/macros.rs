09ef915b40f9be3b9f8d48178c52455c9b93fcdf
Luni-4 <luni-4@hotmail.it>
2022-07-01 13:52:21 +0200

    Use new stable total_cmp function to compare floats

--- src/macros.rs
+++ src/macros.rs
@@ -318,9 +318,8 @@ macro_rules! check_metrics {
 
             $(
                 $(
-                    let value: f64 = $true_float_value;
-                    if !value.is_nan() {
-                        assert!((func_space.metrics.$metric.$func_float() - value).abs() < f64::EPSILON);
+                    if !($true_float_value as f64).is_nan() {
+                        assert!(func_space.metrics.$metric.$func_float().total_cmp(&$true_float_value) == std::cmp::Ordering::Equal);
                     } else {
                         assert!(func_space.metrics.$metric.$func_float().is_nan());
                     }
