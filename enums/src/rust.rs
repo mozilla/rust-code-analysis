73688570c24c75e3afb29eee1dc0d2843a1fd370
Luni-4 <luni-4@hotmail.it>
2022-07-04 13:31:25 +0200

    enums: Fix clippy warnings

--- enums/src/rust.rs
+++ enums/src/rust.rs
@@ -22,7 +22,7 @@ pub fn generate_rust(output: &Path, file_template: &str) -> std::io::Result<()> 
         let name = get_language_name(&lang);
         let c_name = camel_case(name.to_string());
 
-        let file_name = format!("{}.rs", file_template.replace("$", &c_name.to_lowercase()));
+        let file_name = format!("{}.rs", file_template.replace('$', &c_name.to_lowercase()));
         let path = output.join(file_name);
         let mut file = File::create(path)?;
 
