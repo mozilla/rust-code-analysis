41af6a3f025a696f9473dcee3a57100c3c9832e5
Luni-4 <luni-4@hotmail.it>
2023-01-12 12:09:13 +0100

    Update tree-sitter-preproc

--- tree-sitter-preproc/src/parser.c
+++ tree-sitter-preproc/src/parser.c
@@ -5,7 +5,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 13
+#define LANGUAGE_VERSION 14
 #define STATE_COUNT 121
 #define LARGE_STATE_COUNT 16
 #define SYMBOL_COUNT 36
@@ -54,7 +54,7 @@ enum {
   aux_sym_preproc_if_repeat1 = 35,
 };
 
-static const char *ts_symbol_names[] = {
+static const char * const ts_symbol_names[] = {
   [ts_builtin_sym_end] = "end",
   [sym_identifier] = "identifier",
   [sym_nothing] = "nothing",
@@ -93,7 +93,7 @@ static const char *ts_symbol_names[] = {
   [aux_sym_preproc_if_repeat1] = "preproc_if_repeat1",
 };
 
-static TSSymbol ts_symbol_map[] = {
+static const TSSymbol ts_symbol_map[] = {
   [ts_builtin_sym_end] = ts_builtin_sym_end,
   [sym_identifier] = sym_identifier,
   [sym_nothing] = sym_nothing,
@@ -279,12 +279,136 @@ static const TSSymbolMetadata ts_symbol_metadata[] = {
   },
 };
 
-static TSSymbol ts_alias_sequences[PRODUCTION_ID_COUNT][MAX_ALIAS_SEQUENCE_LENGTH] = {
+static const TSSymbol ts_alias_sequences[PRODUCTION_ID_COUNT][MAX_ALIAS_SEQUENCE_LENGTH] = {
   [0] = {0},
 };
 
-static uint16_t ts_non_terminal_alias_map[] = {
+static const uint16_t ts_non_terminal_alias_map[] = {
   0,
+};
+
+static const TSStateId ts_primary_state_ids[STATE_COUNT] = {
+  [0] = 0,
+  [1] = 1,
+  [2] = 2,
+  [3] = 3,
+  [4] = 3,
+  [5] = 2,
+  [6] = 3,
+  [7] = 2,
+  [8] = 8,
+  [9] = 9,
+  [10] = 10,
+  [11] = 11,
+  [12] = 8,
+  [13] = 13,
+  [14] = 8,
+  [15] = 15,
+  [16] = 16,
+  [17] = 17,
+  [18] = 18,
+  [19] = 19,
+  [20] = 20,
+  [21] = 21,
+  [22] = 22,
+  [23] = 23,
+  [24] = 24,
+  [25] = 25,
+  [26] = 26,
+  [27] = 27,
+  [28] = 28,
+  [29] = 29,
+  [30] = 21,
+  [31] = 22,
+  [32] = 24,
+  [33] = 25,
+  [34] = 26,
+  [35] = 21,
+  [36] = 27,
+  [37] = 20,
+  [38] = 19,
+  [39] = 25,
+  [40] = 18,
+  [41] = 23,
+  [42] = 28,
+  [43] = 27,
+  [44] = 16,
+  [45] = 29,
+  [46] = 17,
+  [47] = 22,
+  [48] = 17,
+  [49] = 20,
+  [50] = 23,
+  [51] = 19,
+  [52] = 26,
+  [53] = 18,
+  [54] = 28,
+  [55] = 24,
+  [56] = 16,
+  [57] = 29,
+  [58] = 58,
+  [59] = 59,
+  [60] = 59,
+  [61] = 58,
+  [62] = 58,
+  [63] = 59,
+  [64] = 64,
+  [65] = 65,
+  [66] = 65,
+  [67] = 65,
+  [68] = 68,
+  [69] = 69,
+  [70] = 70,
+  [71] = 71,
+  [72] = 70,
+  [73] = 73,
+  [74] = 74,
+  [75] = 68,
+  [76] = 70,
+  [77] = 68,
+  [78] = 71,
+  [79] = 79,
+  [80] = 69,
+  [81] = 79,
+  [82] = 74,
+  [83] = 71,
+  [84] = 79,
+  [85] = 69,
+  [86] = 74,
+  [87] = 87,
+  [88] = 87,
+  [89] = 89,
+  [90] = 90,
+  [91] = 89,
+  [92] = 92,
+  [93] = 93,
+  [94] = 94,
+  [95] = 95,
+  [96] = 96,
+  [97] = 97,
+  [98] = 95,
+  [99] = 97,
+  [100] = 94,
+  [101] = 92,
+  [102] = 102,
+  [103] = 103,
+  [104] = 103,
+  [105] = 90,
+  [106] = 89,
+  [107] = 107,
+  [108] = 107,
+  [109] = 95,
+  [110] = 97,
+  [111] = 94,
+  [112] = 107,
+  [113] = 92,
+  [114] = 103,
+  [115] = 87,
+  [116] = 90,
+  [117] = 96,
+  [118] = 93,
+  [119] = 96,
+  [120] = 93,
 };
 
 static bool ts_lex(TSLexer *lexer, TSStateId state) {
@@ -949,7 +1073,7 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static TSLexMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0, .external_lex_state = 1},
   [1] = {.lex_state = 71, .external_lex_state = 1},
   [2] = {.lex_state = 10, .external_lex_state = 1},
@@ -1077,17 +1201,17 @@ enum {
   ts_external_token_raw_string_literal = 0,
 };
 
-static TSSymbol ts_external_scanner_symbol_map[EXTERNAL_TOKEN_COUNT] = {
+static const TSSymbol ts_external_scanner_symbol_map[EXTERNAL_TOKEN_COUNT] = {
   [ts_external_token_raw_string_literal] = sym_raw_string_literal,
 };
 
-static bool ts_external_scanner_states[2][EXTERNAL_TOKEN_COUNT] = {
+static const bool ts_external_scanner_states[2][EXTERNAL_TOKEN_COUNT] = {
   [1] = {
     [ts_external_token_raw_string_literal] = true,
   },
 };
 
-static uint16_t ts_parse_table[LARGE_STATE_COUNT][SYMBOL_COUNT] = {
+static const uint16_t ts_parse_table[LARGE_STATE_COUNT][SYMBOL_COUNT] = {
   [0] = {
     [ts_builtin_sym_end] = ACTIONS(1),
     [sym_identifier] = ACTIONS(1),
@@ -1491,7 +1615,7 @@ static uint16_t ts_parse_table[LARGE_STATE_COUNT][SYMBOL_COUNT] = {
   },
 };
 
-static uint16_t ts_small_parse_table[] = {
+static const uint16_t ts_small_parse_table[] = {
   [0] = 2,
     ACTIONS(196), 2,
       sym_raw_string_literal,
@@ -2495,7 +2619,7 @@ static uint16_t ts_small_parse_table[] = {
       sym_identifier,
 };
 
-static uint32_t ts_small_parse_table_map[] = {
+static const uint32_t ts_small_parse_table_map[] = {
   [SMALL_STATE(16)] = 0,
   [SMALL_STATE(17)] = 19,
   [SMALL_STATE(18)] = 38,
@@ -2603,7 +2727,7 @@ static uint32_t ts_small_parse_table_map[] = {
   [SMALL_STATE(120)] = 1217,
 };
 
-static TSParseActionEntry ts_parse_actions[] = {
+static const TSParseActionEntry ts_parse_actions[] = {
   [0] = {.entry = {.count = 0, .reusable = false}},
   [1] = {.entry = {.count = 1, .reusable = false}}, RECOVER(),
   [3] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_translation_unit, 0),
@@ -2809,7 +2933,7 @@ void tree_sitter_preproc_external_scanner_deserialize(void *, const char *, unsi
 #endif
 
 extern const TSLanguage *tree_sitter_preproc(void) {
-  static TSLanguage language = {
+  static const TSLanguage language = {
     .version = LANGUAGE_VERSION,
     .symbol_count = SYMBOL_COUNT,
     .alias_count = ALIAS_COUNT,
@@ -2820,19 +2944,19 @@ extern const TSLanguage *tree_sitter_preproc(void) {
     .production_id_count = PRODUCTION_ID_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
-    .parse_table = (const uint16_t *)ts_parse_table,
-    .small_parse_table = (const uint16_t *)ts_small_parse_table,
-    .small_parse_table_map = (const uint32_t *)ts_small_parse_table_map,
+    .parse_table = &ts_parse_table[0][0],
+    .small_parse_table = ts_small_parse_table,
+    .small_parse_table_map = ts_small_parse_table_map,
     .parse_actions = ts_parse_actions,
     .symbol_names = ts_symbol_names,
     .symbol_metadata = ts_symbol_metadata,
     .public_symbol_map = ts_symbol_map,
     .alias_map = ts_non_terminal_alias_map,
-    .alias_sequences = (const TSSymbol *)ts_alias_sequences,
+    .alias_sequences = &ts_alias_sequences[0][0],
     .lex_modes = ts_lex_modes,
     .lex_fn = ts_lex,
     .external_scanner = {
-      (const bool *)ts_external_scanner_states,
+      &ts_external_scanner_states[0][0],
       ts_external_scanner_symbol_map,
       tree_sitter_preproc_external_scanner_create,
       tree_sitter_preproc_external_scanner_destroy,
@@ -2840,6 +2964,7 @@ extern const TSLanguage *tree_sitter_preproc(void) {
       tree_sitter_preproc_external_scanner_serialize,
       tree_sitter_preproc_external_scanner_deserialize,
     },
+    .primary_state_ids = ts_primary_state_ids,
   };
   return &language;
 }
