8b5a59b50c4933b7ec0c01c899baebd4e4ca30f9
moz.tools <moz-tools-bot@moz.tools>
2023-01-13 11:38:38 +0000

    Deploy rust-code-analysis

--- enums/templates/foo.rs
+++
@@ -1,8 +0,0 @@
-// Generated. DON'T MODIFY BY HAND!
-
-#[derive(Debug, PartialEq)]
-pub enum {{ c_name }} {
-    {% for (name, _, _) in names %}
-    {{ name }} = {{ loop.index }};
-    {% endfor %}
-}
