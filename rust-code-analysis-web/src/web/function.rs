951bed89c7aca1392555387a991267c45b3d841c
Luni-4 <luni-4@hotmail.it>
2023-05-19 11:27:59 +0200

    web: Document function payload and response

--- rust-code-analysis-web/src/web/function.rs
+++ rust-code-analysis-web/src/web/function.rs
@@ -3,28 +3,40 @@ use serde_json::{self, Value};
 
 use rust_code_analysis::{function, Callback, FunctionSpan, ParserTrait};
 
+/// Payload containing source code with function spans to be retrieved.
 #[derive(Debug, Deserialize, Serialize)]
 pub struct WebFunctionPayload {
+    /// Payload identifier.
     pub id: String,
+    /// Source code filename.
     pub file_name: String,
+    /// Source code with function spans to be retrieved.
     pub code: String,
 }
 
+/// Server response containing function spans for the requested source code.
 #[derive(Debug, Serialize)]
 pub struct WebFunctionResponse {
+    /// Server response identifier.
     pub id: String,
+    /// Function spans for the requested source code.
     pub spans: Vec<FunctionSpan>,
 }
 
+/// Source code information.
 #[derive(Debug, Deserialize)]
 pub struct WebFunctionInfo {
+    /// Source code filename.
     pub file_name: String,
 }
 
+/// Server request configuration.
 pub struct WebFunctionCfg {
+    /// Request identifier.
     pub id: String,
 }
 
+/// Unit structure to implement the `Callback` trait.
 pub struct WebFunctionCallback;
 
 impl Callback for WebFunctionCallback {
