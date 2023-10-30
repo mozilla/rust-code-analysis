dd7d98bfd058bce0513cb94703e3bd404c6acf5c
Michael van Straten <michael@vanstraten.de>
2023-10-30 17:29:42 +0100

    Added test for scope resolution operator

--- src/spaces.rs
+++ src/spaces.rs
@@ -378,3 +378,24 @@ impl Callback for Metrics {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{check_func_space, CppParser};
+
+    #[test]
+    fn c_scope_resolution_operator() {
+        check_func_space::<CppParser, _>(
+            "void Foo::bar(){
+                return;
+            }",
+            "foo.c",
+            |func_space| {
+                insta::assert_json_snapshot!(
+                    func_space.spaces[0].name,
+                    @r###""Foo::bar""###
+                );
+            },
+        );
+    }
+}
