b047076924140105694973a6c52ae03c388b8b11
Alex <100879557+alexle0nte@users.noreply.github.com>
2025-03-28 15:20:16 +0100

    Replace tree-sitter-kotlin with tree-sitter-kotlin-ng 1.1.0

--- enums/src/macros.rs
+++ enums/src/macros.rs
@@ -19,7 +19,7 @@ macro_rules! mk_get_language {
     ( $( ($camel:ident, $name:ident) ),* ) => {
         pub fn get_language(lang: &Lang) -> Language {
             match lang {
-                Lang::Kotlin => tree_sitter_kotlin::language(),
+                Lang::Kotlin => tree_sitter_kotlin_ng::LANGUAGE.into(),
                 Lang::Java => tree_sitter_java::LANGUAGE.into(),
                 Lang::Typescript => tree_sitter_typescript::language_typescript(),
                 Lang::Tsx => tree_sitter_typescript::language_tsx(),
