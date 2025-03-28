3bd4fe44d0ebb2f9fb404be453ab49dd1e58dbb5
Alex <100879557+alexle0nte@users.noreply.github.com>
2025-03-28 16:45:34 +0100

    Bump tree-sitter-ccomment to 0.20.3

--- enums/src/macros.rs
+++ enums/src/macros.rs
@@ -27,7 +27,7 @@ macro_rules! mk_get_language {
                 Lang::Python => tree_sitter_python::LANGUAGE.into(),
                 Lang::Rust => tree_sitter_rust::LANGUAGE.into(),
                 Lang::Preproc => tree_sitter_preproc::LANGUAGE.into(),
-                Lang::Ccomment => tree_sitter_ccomment::language(),
+                Lang::Ccomment => tree_sitter_ccomment::LANGUAGE.into(),
                 Lang::Cpp => tree_sitter_mozcpp::language(),
                 Lang::Mozjs => tree_sitter_mozjs::language(),
             }
